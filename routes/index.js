ab063727524cb3f83330d98e792824ea90237333
Brian O'Neil <brian.oneil@warnermedia.com>
2021-08-30 22:31:29 -0400

    added config loading to project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const config = require('../config.json')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'DevOps Docker Node App' });
+  res.render('index', { title: config.TITLE });
 });
 
 module.exports = router;
